ad59cac82c9b2650d1912f153771b7d78a006efa
Yessenia <ychaiu@gmail.com>
2018-12-09 14:10:42 -0800

    add navbar component (basic, no links)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/map/map';
-
+import NavBar from './components/navbar/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           my main app component
+          <NavBar />
           <Map />
       </div>
     );
