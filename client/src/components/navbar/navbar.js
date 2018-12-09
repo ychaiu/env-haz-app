92573481d2d770ccf0659bdbbcbbb2509e33d68f
Yessenia <ychaiu@gmail.com>
2018-12-09 15:02:13 -0800

    test bootstrap with navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import '../../navbar.css';
 
 class NavBar extends Component {
     render() {
         return (
-            <div className="NavBar">
-            navbar test
+            <div class="navbar navbar-dark bg-dark">
+              <a href="#tool-tip">Tool Tip</a>
+              <a href="#sign-in">Sign In</a>
             </div>
         );
     }
