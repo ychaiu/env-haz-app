d532a82ac75393457f64b01766ab3cc98c71fc31
Yessenia <ychaiu@gmail.com>
2018-12-15 17:40:10 -0800

    temporary formatting of sidebar components, route navbar

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       <div className="App">
           <NavBar />
           <Map />
-          <About />
       </div>
     );
   }
--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import EventFormContainer from '../../containers/eventFormContainer.jsx';
+import About from '../about/about.jsx';
 
 class NavBar extends Component {
     render() {
         return (
-            <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <span class="navbar-brand mb-0 h1">HAZMAP</span>
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                     <li class="nav-item active">
-                        <a href="#about" class="nav-link">About</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="/report-event" class="nav-link">Report</a>
-                    </li>
-                </ul>
-                ))}
-            </div>
+            <Router>
+                <div>
+                    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <span class="navbar-brand mb-0 h1">HAZMAP</span>
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                             <li class="nav-item active">
+                                <Link to="/about" class="nav-link">About</Link>
+                            </li>
+                            <li class="nav-item active">
+                                <Link to="/report-event" class="nav-link">Report</Link>
+                            </li>
+                        </ul>
+                        ))}
+                    </div>
+                    <Route path="/about" component={About} />
+                    <Route path="/report-event" component={EventFormContainer} />
+                </div>
+            </Router>
         );
     }
 }
