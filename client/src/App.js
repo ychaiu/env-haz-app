91b118f6c873b93e2f0e01eb22f7e8f9d6508e12
Yessenia <ychaiu@gmail.com>
2018-12-28 11:38:24 -0800

    remove unused components, add callback to map

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import Map from './components/map/map.jsx';
 import NavBar from './components/navbar/navbar.jsx';
-import EventFormContainer from './containers/eventFormContainer.jsx';
-import About from './components/about/about.jsx';
-
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newMarker: {
+                lat: '',
+                lng: ''
+            }
+        }
+    this.handleNewMarkerCoords = this.handleNewMarkerCoords.bind(this);
+    }
+
+    handleNewMarkerCoords = (data) => {
+        console.log(data);
+        // this.setState (prevState => ({newMarker:
+        //     {...prevState.newMarker, lat: newlat, lng: newlng
+        //     }
+        // }), () => console.log(this.state.newMarker))
+    }
+
   render() {
     return (
       <div className="App">
           <NavBar />
-          <Map />
+          <Map 
+            handleNewMarkerCoords = {this.handleNewMarkerCoords} 
+          />
       </div>
     );
   }
