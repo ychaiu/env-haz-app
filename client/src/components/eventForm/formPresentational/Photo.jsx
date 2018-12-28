2bbe5c4b9c2e246bbe86654b5ad8dac527fc6e31
Yessenia <ychaiu@gmail.com>
2018-12-28 11:39:38 -0800

    add Photo dumb component

---
+++ client/src/components/eventForm/formPresentational/Photo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Photo = (props) => {
+    return (
+        <div className="form-group">
+            <label htmlFor={props.name}>{props.title}</label>
+            <input
+                type={props.type}
+                name={props.name}
+                value={props.value}
+                onChange={props.handleChange}
+             />
+        </div>
+    )
+}
+
+export default Photo;
