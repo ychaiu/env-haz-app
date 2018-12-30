a2cdd0ba35785cbe36b512c57c3a8044210d4bf8
Yessenia <ychaiu@gmail.com>
2018-12-29 22:28:21 -0800

    successfully add simple infowindow for a marker

--- client/src/components/map/map.jsx
+++ client/src/components/map/map.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import config from '../../config/config';
 import mapStyle from './mapStyle';
-import * as clusterMarkerImg from '../../../public/img/clusterMarkers/m2.png'
+// import * as clusterMarkerImg from '../../../public/img/clusterMarkers/m2.png'
 
 const key = config.mapKey;
 
@@ -18,11 +18,26 @@ const loadMarkers = (map, maps) => {
         let eventObj = data[i];
         let marker = new maps.Marker({
           position: new maps.LatLng(eventObj.latitude, eventObj.longitude),
-          map:map
+          map: map,
+        })
+        let infowindow = new maps.InfoWindow({
+          content: 'content'
         });
+        marker.addListener('click', function(){
+          infowindow.open(map,marker);
+        })
       }
-    });
+      })
 }
+
+// const loadInfoWindow = (map, maps) => {
+//   let infowindow = new maps.InfoWindow({
+//     content: 'content'
+//   });
+//   marker.addListener('click', function(){
+//   infowindow.open(map,marker);
+//   })
+// }
 
 class Map extends Component {
     static defaultProps = {
@@ -34,13 +49,13 @@ class Map extends Component {
     }; 
 
   handleApiLoaded (map,maps) {
-    let marker;
+    let newMarker;
     function placeMarkerAndPanTo(latLng) {
-      if (marker) {
-        marker.setPosition(latLng);
+      if (newMarker) {
+        newMarker.setPosition(latLng);
       }
       else {
-        marker = new maps.Marker({
+        newMarker = new maps.Marker({
           position: latLng,
           map:map
         });
@@ -52,6 +67,7 @@ class Map extends Component {
     map.addListener('click', function(e){
       placeMarkerAndPanTo(e.latLng,map);
     });
+
     loadMarkers(map,maps);
 
     // let markerCluster = new MarkerClusterer(map, markers,
